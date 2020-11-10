67f4cd665a7d8f7f1ed673292e17d0505ba35565
suchyan01 <suchayan.biswas@gmail.com>
2020-11-11 04:02:03 +0530

    nothing significant

---
+++ Oct/src/ArraysOrderStatistics/smallest.java
@@ -0,0 +1,16 @@
+package ArraysOrderStatistics;
+
+class smallest{
+
+	public static int kthSmallest(int[] arr, int l, int r, int k) 
+	{ 
+	    //Your code here
+		return 1;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
--- Oct/src/Sorting/MergeSort.java
+++ Oct/src/Sorting/MergeSort.java
@@ -1,24 +1,65 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class MergeSort {
-	
+	static int arrTemp[]= new int[8];
 	public static void main(String[] args) {
 		int[]arr= {4,5,3,1,7,0,4,9};
-		sorted(arr,0,arr.length);
+		sorted(arr,0,arr.length-1);
+		System.out.println(Arrays.toString(arr));
 	}
 
-	 static int[] sorted(int[] arr, int lb, int up) {
-		while(up>lb) {
+	 static void sorted(int[] arr, int lb, int up) {
+		if(lb<up) {
 		int mid=(up+lb)/2;
 		sorted(arr,lb,mid);
-		sorted(arr,mid+1,lb);
+		sorted(arr,mid+1,up);
 		merge(arr,lb,mid,up);
 		}
 		
-		return arr;
+		
 	}
 	
 	static void merge(int arr[],int lb,int mid,int up){
+		
+		
+		int i=lb,j=mid+1,k=lb;
+		
+		while(i<=mid && j<=up) {
+			if(arr[i]<=arr[j]) {
+				arrTemp[k]=arr[i];
+				i++;
+			}
+			else {
+				arrTemp[k]=arr[j];
+				j++;
+			}
+			k++;
+			
+		}
+				  if(i>mid) 
+				  { 
+					  while(j<=up) {
+						  arrTemp[k]=arr[j];
+						  j++;
+						  k++; 
+						  } 
+					  } 
+				  else {
+					  while(i<=mid) { 
+						  arrTemp[k]=arr[i]; 
+						  i++; 
+						  k++; 
+						  } 
+		  }
+		 
+		//System.out.println("111111111"+Arrays.toString(arrTemp));
+		for(i=0;i<=up;i++) {
+			arr[i]=arrTemp[i];
+		}
+		
+		
 		
 	}
 
